2aa903530fb77637a6567ef5835c2ff05efdaa79
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-07-10 07:18:45 +0600

    setup the project ,added jqery cdn,bootstarp cdn

---
+++ resources/views/master.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Jquery,Ajax,Laravel Product CRUD</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <main class="bg-primary">
+    @yield('content')
+  </main>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
+    integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+    integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous">
+  </script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+  </script>
+  @yield('scripts')
+</body>
+
+</html>
---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+@section('content')
+    From Index
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function () {
+            console.log("hello kader")
+        });
+    </script>
+@endsection
